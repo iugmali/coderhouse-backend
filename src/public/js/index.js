6b53729c86755d034d4c37f8b4729f51c05aa31d
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2024-04-09 19:55:42 -0300

    refactor views and chat.

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -6,7 +6,7 @@ socket.on('products', (products) => {
   if (products.length === 0) productsList.innerHTML = '<h1>Nenhum produto cadastrado</h1>';
   products.forEach(product => {
     const productElement = document.createElement('div');
-    productElement.className = 'product';
+    productElement.className = 'product box';
     productElement.innerHTML = `
             <h2>${product.title}</h2>
             <p>Descrição: ${product.description}</p>
