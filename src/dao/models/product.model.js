50a84217e6247d0fd740e0e6781d93b7595c1c87
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2024-04-23 18:37:00 -0300

    feat - add paginate plugin to product model

--- src/dao/models/product.model.js
+++ src/dao/models/product.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
 
 const productSchema = new mongoose.Schema({
   title: {
@@ -43,5 +44,5 @@ const productSchema = new mongoose.Schema({
     default: Date.now
   }
 });
-
+productSchema.plugin(mongoosePaginate);
 export default mongoose.model('products', productSchema);
