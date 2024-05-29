b16b5e2bef6ddcb1c9c86be255ae253be1d43965
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2024-05-28 21:24:48 -0300

    refactor - remove console.log

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -2,7 +2,6 @@ const searchForm = document.getElementById('search-form');
 
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log(window.location.search)
   const params = new URLSearchParams(window.location.search);
   const searchInput = document.getElementById('search-input');
   const searchValue = searchInput.value.trim();
