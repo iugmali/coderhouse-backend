ebc4e1d40ff9eca6ebdee891ccdac96479105ea1
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2024-06-11 23:55:51 -0300

    update middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,3 +4,17 @@ export const checkAuth = (req, res, next) => {
     }
     res.redirect('/login');
 }
+
+export const checkAuthJson = (req, res, next) => {
+    if (req.session && req.session.user) {
+        return next();
+    }
+    res.status(401).json({error: 'Unauthorized'});
+}
+
+export const checkAdminJson = (req, res, next) => {
+    if (req.session && req.session.user && req.session.user.role === 'admin') {
+        return next();
+    }
+    res.status(401).json({error: 'Unauthorized'});
+}
